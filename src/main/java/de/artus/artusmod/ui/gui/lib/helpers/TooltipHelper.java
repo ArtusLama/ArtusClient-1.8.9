451bebd1b70540910c75b8767c5bd76a8d99639d
Artus <artuslama@gmail.com>
2024-05-23 22:43:24 +0200

    Fixed tooltip position

--- src/main/java/de/artus/artusmod/ui/gui/lib/helpers/TooltipHelper.java
+++ src/main/java/de/artus/artusmod/ui/gui/lib/helpers/TooltipHelper.java
@@ -15,17 +15,18 @@ public class TooltipHelper {
     @Getter @Setter
     private static Color tooltipTextColor = Color.of("#D2D2D2");
     @Getter @Setter
-    private static Color tooltipBackgroundColor = Color.of("#1F1F1F").getWithAlpha(220);
+    private static Color tooltipBackgroundColor = Color.of("#1A1A1A").getWithAlpha(220);
 
     public static void drawTooltip(int x, int y, String text, FontRenderer fr) {
         // Offset to prevent the tooltip from being hidden by the cursor
         x += 5;
+        y += 5;
 
         int textWidth = fr.getStringWidth(text);
         int padding = 3;
         int width = textWidth + padding * 2;
         int height = fr.FONT_HEIGHT + padding * 2;
-        int bgRadius = 4;
+        int bgRadius = 2;
 
         int displayWidth = Minecraft.getMinecraft().currentScreen.width;
         if (x + width > displayWidth) x = displayWidth - width; // fix out of screen
